f2bc790d51cc8baa6a81b9dcb173c6ea3b0d8b5d
marcobuontempo <marco.buon36@gmail.com>
2024-11-20 19:06:15 +1100

    Add scrollbar styles

--- src/components/common/MainWrapper.tsx
+++ src/components/common/MainWrapper.tsx
@@ -9,7 +9,7 @@ interface Props extends HTMLAttributes<HTMLElement> {
 export default function MainWrapper({ title, baseURL, redirectURL, className, children, ...props }: Props) {
   return (
     <main
-      className={`flex flex-col overflow-x-hidden flex-1 text-white rounded-2xl ${className}`}
+      className={`min-h-[960px] flex flex-col overflow-x-hidden flex-1 text-white rounded-2xl ${className}`}
       {...props}
     >
       <h2 className='text-center text-2xl tracking-widest font-bold py-5 rounded-t-2xl border-2 border-b-0 border-white bg-white/10 overflow-hidden whitespace-nowrap'>{title}</h2>
--- src/components/common/Table.tsx
+++ src/components/common/Table.tsx
@@ -8,6 +8,7 @@ import {
 import LoadingSpinner from './LoadingSpinner';
 import { Dispatch } from 'react';
 import RetryButton from './RetryButton';
+import arrow from '@assets/icons/arrow.svg';
 
 interface Props<T> {
   data: T[] | undefined;
@@ -30,7 +31,7 @@ export default function Table<T>({ columns, data, pageCount, pageIndex, setPageI
   } else if (isError) {
     content = <RetryButton refetch={refetch} />;
   } else if (!data || data?.length === 0) {
-    content = <p>No data</p>;
+    content = <p className='text-center italic'>No data available.</p>;
   } else {
     const table = useReactTable({
       data,
@@ -41,52 +42,98 @@ export default function Table<T>({ columns, data, pageCount, pageIndex, setPageI
     });
 
     content = (
-      <table className={className}>
-        <thead className='font-montserrat font-bold text-left'>
-          {
-            table.getHeaderGroups().map(header => {
-              return <tr key={header.id} className='sticky top-0'>{header.headers.map(cell => {
-                return <th key={cell.id} colSpan={cell.colSpan} className='whitespace-nowrap pt-4 pb-3.5 bg-white/15 first:rounded-tl-2xl first:pl-6 last:rounded-tr-2xl last:pr-6'>
-                  {flexRender(cell.column.columnDef.header, cell.getContext())}
-                </th>
-              })}
-              </tr>
-            })
-          }
-        </thead>
+      <>
+        <div className='overflow-x-auto overflow-y-hidden scroll-smooth'>
+          <table className='mb-4'>
+            <thead className='font-montserrat font-bold text-left'>
+              {
+                table.getHeaderGroups().map(header => {
+                  return <tr key={header.id} className='sticky top-0'>{header.headers.map(cell => {
+                    return <th key={cell.id} colSpan={cell.colSpan} className='whitespace-nowrap pt-4 pb-3.5 bg-white/15 first:rounded-tl-2xl first:pl-6 last:rounded-tr-2xl last:pr-6'>
+                      {flexRender(cell.column.columnDef.header, cell.getContext())}
+                    </th>
+                  })}
+                  </tr>
+                })
+              }
+            </thead>
 
-        <tbody className='font-ibm-plex-mono'>
-          {
-            table.getRowModel().rows.map((row, rowNumber, rows) => (
-              <tr key={row.id} className='relative text-white/50'>
-                {
-                  row.getVisibleCells().map((cell, cellNumber, cells) => (
-                    <td key={cell.id} className={`relative whitespace-nowrap bg-white/10 p-4 first:pl-6 last:pr-6 ${rowNumber === rows.length - 1 && 'first:rounded-bl-2xl last:rounded-br-2xl'}`}>
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                      {
-                        // right border. for every cell except last
-                        cellNumber !== cells.length - 1 &&
-                        <span className='absolute top-0 bottom-0 right-0 w-0.5 h-full py-2 after:block after:w-full after:h-full after:rounded-full after:bg-white/5'></span>
-                      }
-                      {
-                        // bottom border. for every row except last
-                        rowNumber !== rows.length - 1 &&
-                        <span className={`absolute bottom-0 left-0 right-0 w-full h-0.5 after:block after:w-full after:h-full after:bg-white/5 ${cellNumber === 0 && 'pl-2 after:rounded-l-full'} ${cellNumber === cells.length - 1 && 'pr-2 after:rounded-r-full'} `}></span>
-                      }
-                    </td>
-                  ))
-                }
-              </tr>
-            ))
-          }
-        </tbody>
-      </table >
+            <tbody className='font-ibm-plex-mono'>
+              {
+                table.getRowModel().rows.map((row, rowNumber, rows) => (
+                  <tr key={row.id} className='relative text-white/50'>
+                    {
+                      row.getVisibleCells().map((cell, cellNumber, cells) => (
+                        <td key={cell.id} className={`relative whitespace-nowrap bg-white/10 p-4 first:pl-6 last:pr-6 ${rowNumber === rows.length - 1 && 'first:rounded-bl-2xl last:rounded-br-2xl'}`}>
+                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                          {
+                            // right border. for every cell except last
+                            cellNumber !== cells.length - 1 &&
+                            <span className='absolute top-0 bottom-0 right-0 w-0.5 h-full py-2 after:block after:w-full after:h-full after:rounded-full after:bg-white/5'></span>
+                          }
+                          {
+                            // bottom border. for every row except last
+                            rowNumber !== rows.length - 1 &&
+                            <span className={`absolute bottom-0 left-0 right-0 w-full h-0.5 after:block after:w-full after:h-full after:bg-white/5 ${cellNumber === 0 && 'pl-2 after:rounded-l-full'} ${cellNumber === cells.length - 1 && 'pr-2 after:rounded-r-full'} `}></span>
+                          }
+                        </td>
+                      ))
+                    }
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table >
+        </div>
+        {
+          (pageIndex !== undefined && setPageIndex !== undefined && pageCount !== undefined)
+            ? <div className='w-full flex justify-center items-center font-ibm-plex-mono py-4'>
+              <button
+                className='-rotate-90 px-1 disabled:opacity-40'
+                onClick={() => setPageIndex(pageIndex - 1)}
+                disabled={pageIndex <= 0 || isPending || isError || !data}
+              >
+                <img src={arrow} alt='previous page' />
+              </button>
+              <button
+                className='-rotate-90 px-1 disabled:opacity-40'
+                onClick={() => setPageIndex(0)}
+                disabled={pageIndex <= 0 || isPending || isError || !data}
+              >
+                <img src={arrow} alt='first page' />
+                <img src={arrow} alt='first page' />
+              </button>
+              <p
+                className='font-medium text-white/80 px-6'
+              >
+                {pageIndex + (pageCount === 0 ? 0 : 1)} <span className='font-normal'>of</span> {pageCount.toLocaleString()}
+              </p>
+              <button
+                className='rotate-90 px-1 disabled:opacity-40'
+                onClick={() => setPageIndex(pageCount - 1)}
+                disabled={pageIndex >= pageCount - 1 || isPending || isError || !data}
+              >
+                <img src={arrow} alt='last page' />
+                <img src={arrow} alt='last page' />
+              </button>
+              <button
+                className='rotate-90 px-1 disabled:opacity-40'
+                onClick={() => setPageIndex(pageIndex + 1)}
+                disabled={pageIndex >= pageCount - 1 || isPending || isError || !data}
+              >
+                <img src={arrow} alt='next page' />
+              </button>
+            </div>
+            : null
+        }
+      </>
+
     )
   }
 
   return (
-    <>
+    <div className={`${className}`}>
       {content}
-    </>
+    </div>
   )
 }
